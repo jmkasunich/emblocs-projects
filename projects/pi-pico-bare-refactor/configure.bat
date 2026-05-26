bdff88c0b3edb6b1f627919eb7040cf730e715cc
John Kasunich <jmkasunich@fastmail.fm>
2026-05-25 23:27:04 -0400

    add automatic run of blocs_compiler.py to build system

--- projects/pi-pico-bare-refactor/configure.bat
+++ projects/pi-pico-bare-refactor/configure.bat
@@ -1,6 +1,5 @@
 @echo off
 if exist build rmdir /s /q build
 mkdir build
-python ..\..\submodules\emblocs\python\variant_compiler.py blink.blocs build\
 cd build
 cmake -G Ninja -DCMAKE_MAKE_PROGRAM=ninja ..
